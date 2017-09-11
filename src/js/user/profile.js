bdd92c8942fd3dd2e147d302af37fe93d9cab882
Hero <super908@163.com>
2017-09-11 09:13:34 +0800

    个人资料数据回显与设置--附加修改密码

--- src/js/user/profile.js
+++ src/js/user/profile.js
@@ -7,8 +7,21 @@ $.ajax({
   url:'/v6/teacher/profile',
   type:'get',
   success:function(data){
+    console.log(data)
     if(data.code == 200){
       $('.settings').html(template('settings-tpl',data.result));
+      
     }
   }
 });
+
+//提交表单
+$('#profile-form').ajaxForm({
+  delegation:true,
+  success:function(data){
+    if(data.code == 200){
+      alert("修改成功");
+      location.href = '/dist'
+    }
+  }
+})
--- src/js/user/repass.js
+++ src/js/user/repass.js
@@ -1,2 +1,17 @@
 require('../common/aside.js');
 require('../common/header.js');
+
+
+//修改密码
+$('.form-horizontal').on('submit',function(){
+  if($("#pass-word").val() != $("#new-pass-word").val()){
+    alert('密码不一致,请重新输入');
+    return false;
+  }
+  $('.form-horizontal').ajaxSubmit({
+    success:function(data){
+      console.log(data)
+    }
+  });
+  return false;
+})
