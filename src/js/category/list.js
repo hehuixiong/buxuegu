a7517271dc2358c8539355c51227ea4d849020df
Hero <super908@163.com>
2017-09-11 11:05:28 +0800

    科目列表数据渲染

--- src/js/category/list.js
+++ src/js/category/list.js
@@ -1,2 +1,8 @@
 require('../common/aside.js');
 require('../common/header.js');
+
+//学科列表数据渲染
+
+$.get('/v6/category',function(data){
+  $(".table-bordered").append(template("category-list-tpl",data.result));
+})
