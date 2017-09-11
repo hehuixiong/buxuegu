741d1b646c0a764c9bcb08656c5b7943beddc968
Hero <super908@163.com>
2017-09-11 21:21:07 +0800

    讲师管理-渲染\添加\编辑\与注销和启用

--- src/js/teacher/edit.js
+++ src/js/teacher/edit.js
@@ -1,2 +1,20 @@
 require('../common/aside.js');
 require('../common/header.js');
+var edit = require('../common/tool.js');
+//编辑讲师动态渲染
+//获取当然页面的id
+var tc_id = edit.getSearch("tc_id");
+$.get('/v6/teacher/edit',{tc_id:tc_id},function(data){
+  $(".teacher-edit-form").html(template('edit-teacher-tpl',data.result));
+})
+
+//确认修改讲师
+$("#teacher-edit-form").ajaxForm({
+  delegation:true,
+  success:function(data){
+    if(data.code == 200){
+      alert('修改讲师成功');
+      location.href = '/dist/html/teacher/list.html';
+    }
+  }
+});
--- src/js/teacher/list.js
+++ src/js/teacher/list.js
@@ -1,2 +1,25 @@
 require('../common/aside.js');
 require('../common/header.js');
+//讲师列表展示
+$.get("/v6/teacher",function(data){
+  $("#tbody-teacher-list").html(template("teacher-list-tpl",data.result));
+});
+
+//查看讲师渲染
+
+//????????????
+
+
+//注销与启用讲师
+$(document).on("click","#btn-teacher",function(){
+  var $this = $(this);
+  var data = {
+    tc_id:$(this).attr('data-id'),
+    tc_status:$(this).attr("data-status")
+  };
+  $.post("/v6/teacher/handle",data,function(data){
+    var newStatus = data.result.tc_status;
+    $this.text(newStatus==0?"注 销":"启 用");
+    $this.attr('data-status',newStatus);
+  })
+})
